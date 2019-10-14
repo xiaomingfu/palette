7f3fd833bcc890124d2e097fcbaac4a5c80f8561
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-14 15:41:36 -0700

    fix plaetteList key bug

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -41,8 +41,8 @@ class PaletteList extends Component {
                         <h1>React Colors</h1>
                     </nav>
                     <div className={classes.palettes}>
-                        {palettes.map(p => (
-                            <MiniPalette {...p} />))
+                        {palettes.map((p, idx) => (
+                            <MiniPalette {...p} key={idx} />))
                         }
                     </div>
                 </div>
