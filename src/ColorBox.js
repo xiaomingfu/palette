30ce6ad18bb0759596f17df850e62cbe7e008d90
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-16 00:41:11 -0700

    refactory colorbox

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -16,7 +16,8 @@ const styles = {
         textTransform: "uppercase",
         marginBottom: "-3.5px",
         "&:hover button": {
-            opacity: "1"
+            opacity: "1",
+            transition: "0.5s"
         },
     },
     copyText: {
@@ -56,6 +57,67 @@ const styles = {
         border: "none",
         textDecoration: "none",
         opacity: "0",
+    },
+    boxContent: {
+        position: "absolute",
+        width: "100%",
+        left: "0px",
+        bottom: "0px",
+        padding: "10px",
+        color: "black",
+        letterSpacing: "1px",
+        fontSize: "12px",
+    },
+    copyOverLay: {
+        width: "100%",
+        height: "100%",
+        opacity: "0",
+        zIndex: "0",
+        position: "absolute",
+        transition: "transform 0.5s ease-in-out",
+        transform: "scale(0.1)",
+    },
+    showOverlay: {
+        opacity: "1",
+        transform: "scale(50)",
+        zIndex: "10",
+        position: "absolute",
+    },
+    copyMeg: {
+        position: "fixed",
+        left: "0",
+        right: "0",
+        top: "0",
+        bottom: "0",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        fontSize: "4rem",
+        transform: "scale(0.1)",
+        opacity: "0",
+        color: "white",
+        "& h1": {
+            fontWeight: "400",
+            textshadow: "1px 2px black",
+            background: "rgba(255, 255, 255, 0.5)",
+            width: "100%",
+            textAlign: "center",
+            padding: "1rem",
+            marginBottom: "0",
+            textTransform: "uppercase",
+        },
+        "& p": {
+            fontSize: "2rem",
+            fontWeight: "100",
+        }
+    },
+    showMsg: {
+        opacity: "1",
+        transform: "scale(1)",
+        zIndex: "25",
+        transition: "all 0.4s ease-in-out",
+        transitionDelay: "0.3s",
     }
 }
 class ColorBox extends Component {
@@ -75,14 +137,14 @@ class ColorBox extends Component {
         return (
             <CopyToClipboard text={color} onCopy={this.handleCopy}>
                 <div style={{ background: color }} className={classes.colorBox}>
-                    <div className={`copy-overlay ${copied && "show"}`}
+                    <div className={`${classes.copyOverLay} ${copied && classes.showOverlay}`}
                         style={{ background: color }} />
-                    <div className={`copy-msg ${copied && 'show'}`}>
+                    <div className={`${classes.copyMeg} ${copied && classes.showMsg}`}>
                         <h1>Copied!</h1>
                         <p className={classes.copyText}>{color}</p>
                     </div>
                     <div className="copy-container">
-                        <div className="box-content">
+                        <div className={classes.boxContent}>
                             <span className={classes.colorName}>{name}</span>
                         </div>
                         <button className={classes.copyBtn}>Copy</button>
