9ed270c1907e1ebaa72be4be1470872403f6f509
Xiaoming Fu <xmingfu86@gmail.com>
2019-10-19 15:20:41 -0700

    make palette responsive

--- src/styles/PaletteListStyle.js
+++ src/styles/PaletteListStyle.js
@@ -1,3 +1,4 @@
+import sizes from "./sizes";
 export default {
   root: {
     backgroundColor: "blue",
@@ -11,7 +12,13 @@ export default {
     display: "flex",
     alignItems: "flex-start",
     flexDirection: "column",
-    flexWrap: "wrap"
+    flexWrap: "wrap",
+    [sizes.down("lg")]: {
+      width: "80%"
+    },
+    [sizes.down("xs")]: {
+      width: "75%"
+    }
   },
   nav: {
     display: "flex",
@@ -28,6 +35,15 @@ export default {
     width: "100%",
     display: "grid",
     gridTemplateColumns: "repeat(3, 30%)",
-    gridGap: "5%"
+    gridGap: "2.5rem",
+
+    [sizes.down("md")]: {
+      gridTemplateColumns: "repeat(2, 50%)",
+      gridGap: "1.5rem"
+    },
+    [sizes.down("xs")]: {
+      gridTemplateColumns: "repeat(1, 100%)",
+      gridGap: "1.5rem"
+    }
   }
 };
